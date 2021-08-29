5a19d7dce557b740a7a97f141cb00a7f55eba12f
flix <flixnflux@gmail.com>
2021-08-29 15:34:08 +0200

    fixing typos

--- root/etc/cont-init.d/15-enforce_proxy.sh
+++ root/etc/cont-init.d/15-enforce_proxy.sh
@@ -6,10 +6,17 @@ if [ -f /usr/bin/apt ]; then
 elif [ -f /sbin/apk ]; then
   apk add --no-cache ufw
 fi
-echo ufw allow in to $LOCA_SUBNET
-echo ufw allow out to ${LOCA_SUBNET}
+echo ufw allow in to ${LOCAL_SUBNET}
+ufw allow in to ${LOCAL_SUBNET}
+echo ufw allow out to ${LOCAL_SUBNET}
+ufw allow out to ${LOCAL_SUBNET}
 echo ufw default deny outgoing
+ufw default deny outgoing
 echo ufw default deny incoming
+ufw default deny incoming
 echo ufw allow out to ${PROXY_IP} port ${PROXY_PORT}
+ufw allow out to ${PROXY_IP} port ${PROXY_PORT}
 echo ufw allow out to ${DNS_SERVER1} port 53 proto udp
+ufw allow out to ${DNS_SERVER1} port 53 proto udp
 echo ufw allow out to ${DNS_SERVER2} port 53 proto udp
+ufw allow out to ${DNS_SERVER2} port 53 proto udp
