4023ef939b9ae6c84d23e1a63e1717c46c238b24
flix <flixnflux@gmail.com>
2021-08-29 12:22:10 +0200

    Initial commit

---
+++ root/etc/cont-init.d/15-enforce_proxy.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/with-contenv bash
+
+if [ -f /usr/bin/apt ]; then
+  apt-get install ufw
+elif [ -f /sbin/apk ]; then
+  apk add --no-cache ufw
+fi
+ufw allow in to ${LOCA_SUBNET}
+ufw allow out to ${LOCA_SUBNET}
+ufw default deny outgoing
+ufw default deny incoming
+ufw allow out to ${PROXY_IP} port ${PROXY_PORT}
+ufw allow out to ${DNS_SERVER1} port 53 proto udp
+ufw allow out to ${DNS_SERVER2} port 53 proto udp
