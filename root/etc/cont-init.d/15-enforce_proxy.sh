2d9b6eba458e44a6f5b1766847adf7c7d04aa5ea
flix <flixnflux@gmail.com>
2021-08-29 15:40:18 +0200

    Testing sudo

--- root/etc/cont-init.d/15-enforce_proxy.sh
+++ root/etc/cont-init.d/15-enforce_proxy.sh
@@ -7,16 +7,16 @@ elif [ -f /sbin/apk ]; then
   apk add --no-cache ufw
 fi
 echo ufw allow in to ${LOCAL_SUBNET}
-ufw allow in to ${LOCAL_SUBNET}
+sudo ufw allow in to ${LOCAL_SUBNET}
 echo ufw allow out to ${LOCAL_SUBNET}
-ufw allow out to ${LOCAL_SUBNET}
+sudo ufw allow out to ${LOCAL_SUBNET}
 echo ufw default deny outgoing
-ufw default deny outgoing
+sudo ufw default deny outgoing
 echo ufw default deny incoming
-ufw default deny incoming
+sudo ufw default deny incoming
 echo ufw allow out to ${PROXY_IP} port ${PROXY_PORT}
-ufw allow out to ${PROXY_IP} port ${PROXY_PORT}
+sudo ufw allow out to ${PROXY_IP} port ${PROXY_PORT}
 echo ufw allow out to ${DNS_SERVER1} port 53 proto udp
-ufw allow out to ${DNS_SERVER1} port 53 proto udp
+sudo ufw allow out to ${DNS_SERVER1} port 53 proto udp
 echo ufw allow out to ${DNS_SERVER2} port 53 proto udp
-ufw allow out to ${DNS_SERVER2} port 53 proto udp
+sudo ufw allow out to ${DNS_SERVER2} port 53 proto udp
